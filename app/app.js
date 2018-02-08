be791446173a158c49ea16e40429dd7b2d194e41
schmidt <henri.schmidt@adesso.de>
2018-02-08 23:03:59 +0100

    add basic app

---
+++ app/app.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <Navigation />
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
