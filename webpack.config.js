5a054eb6bffafb49c23bcfef1f6ab4a20b123770
schmidt <henri.schmidt@adesso.de>
2018-02-08 23:03:42 +0100

    add basic config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './app/app.js',
+    output: {
+        filename: 'bundle.js',
+        publicPath: '/assets/',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /.*\.js$/,
+                include: path.resolve(__dirname, 'app'),
+                loader: "babel-loader",
+                options: {
+                    presets: ["env", "react", "stage-2"]
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: "./",
+    }
+};
