b25d02ceead612d29f5e7a11fdff84c9057d29d0
hesch <github@henri.mails-schmidt.de>
2018-02-13 23:22:32 +0100

    add proxy to horriblesubs

--- webpack.config.js
+++ webpack.config.js
@@ -21,5 +21,12 @@ module.exports = {
     },
     devServer: {
         contentBase: "./",
+        proxy: {
+            '/feed': {
+                target: 'http://horriblesubs.info/rss.php?res=all',
+                changeOrigin: true,
+                pathRewrite: {'^/feed' : ''},
+            }
+        }
     }
 };
